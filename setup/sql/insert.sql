74178653272e73ed39f7c1d95fdf87617d57b3c1
Allison Ines <allisonines@allisons-mbp.attlocal.net>
2020-04-18 13:22:57 -0700

    switch to double quote

--- setup/sql/insert.sql
+++ setup/sql/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO regular_hymns_dev (`hymn_num`, `title`, `revision_date`) VALUES ({hymn_num}, '{title}', '{revision_date}');
+INSERT INTO regular_hymns_dev (`hymn_num`, `title`, `revision_date`) VALUES ({hymn_num}, "{title}", "{revision_date}");
